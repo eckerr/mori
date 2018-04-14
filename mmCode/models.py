b21ddea2f652d673a6d487888c043b951d957513
Ed Kerr <edkerr@comcast.net>
2018-04-13 19:26:27 -0500

    saving models  pre foreign key

--- mmCode/models.py
+++ mmCode/models.py
@@ -1,24 +1,62 @@
 from app import db
+from sqlalchemy import ForeignKey
 
 class PersonEntry(db.Model):
+    STATUS_LIVING = 0
+    STATUS_DECEASED = 1
+    GENDER_MALE = 0
+    GENDER_FEMALE = 1
+
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(60))
     middle_name = db.Column(db.String(60))
     last_name = db.Column(db.String(60))
     suffix = db.Column(db.String(10))
-    gender = db.Column(db.String(1))
+    gender = db.Column(db.SmallInteger, default=GENDER_MALE)
+    status = db.Column(db.SmallInteger, default=STATUS_DECEASED)
     day_of_birth_known = db.Column(db.String(1))
     month_of_birth_known = db.Column(db.String(1))
     year_of_birth_known = db.Column(db.String(1))
     day_of_death_known = db.Column(db.String(1))
     month_of_death_known = db.Column(db.String(1))
     year_of_death_known = db.Column(db.String(1))
+    date_of_birth = db.Column(db.String(6))
+    date_of_death = db.Column(db.String(6))
+    # Cemetery
+    cemetery_id = db.relationship('CemeteryEntry',
+        backref='person_entry', lazy=True, uselist=False)
+    # grave location
+    grave_location = db.Column(db.Text)
+    grave_GPS_lat = db.Column(db.Float)
+    grave_GPS_lon = db.Column(db.Float)
+    # pictures
+    pic_thumb = db.Column(db.String(60))
+    pic_gstone = db.Column(db.String(60))
+    pic_fav = db.Column(db.String(60))
+    pic_other = db.Column(db.String(60))
+    pic_description = db.Column(db.Text)
+    pic_notes = db.Column(db.Text)
+    pic_bio = db.Column(db.Text)
+
+class CemeteryEntry(db.Model):
     # cemetery
+    id = db.Column(db.Integer, primary_key=True)
     name_of_cemetery = db.Column(db.String(100))
     street_of_cemetery = db.Column(db.String(100))
     city_of_cemetery = db.Column(db.String(100))
     state_of_cemetery = db.Column(db.String(2))
     zip_of_cemetery = db.Column(db.String(10))
-    # grave location
-    grave_location = db.Column(db.Text)
-    grave_GPS_lat = db.Column(db.Float)
+    person_id = db.Column(db.Integer, db.ForeignKey('person_entry.id'),
+                                           nullable=True)
+
+    def __repr__ (self):
+        return "<CemeteryEntry:(cemetery= '%s',\
+                               street= '%s',\
+                               city= '%s',\
+                               state= '%s',\
+                               zip= '%s')" %\
+                               (self.name_of_cemetery,\
+                               self.street_of_cemetery,\
+                               self.city_of_cemetery,\
+                               self.state_of_cemetery,\
+                               self.zip_of_cemetery)
